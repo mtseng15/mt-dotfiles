c6de7dfb7f4f68c79119cd657236204e0358bb0c
mtseng15 <tseng.micah@gmail.com>
2024-11-03 15:33:45 -0600

    added grep bindings

--- nvim/.config/nvim/lua/mt-config/lazy/fzf-lua.lua
+++ nvim/.config/nvim/lua/mt-config/lazy/fzf-lua.lua
@@ -5,7 +5,10 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
         -- calling `setup` is optional for customization
-        require("fzf-lua").setup({})
+        require("fzf-lua").setup({
+
+        })
         vim.keymap.set("n", "<C-f>", require("fzf-lua").files, { desc = "Fzf Files" })
+        vim.keymap.set("n", "<C-g>", require("fzf-lua").grep_project, { desc = "Grep Files" })
     end
 }

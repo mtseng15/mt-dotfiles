7af88f824462e3370f519d944ab4cfa3f39acada
Micah Tseng <micahtseng@pop-os.localdomain>
2026-06-11 09:28:29 -0500

    latest changes on linux machine

--- nvim/.config/nvim/lua/mt-config/remap.lua
+++ nvim/.config/nvim/lua/mt-config/remap.lua
@@ -1,2 +1,5 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>n", vim.diagnostic.goto_next)
+vim.keymap.set("n", "<leader>b", vim.diagnostic.goto_prev)
+
